6e1b31c5007ff7c5fb610ebac7b3a9097fea30c8
BREBION Mathis <mathis.brebion@icloud.com>
2023-01-06 14:20:56 +0100

    main

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "src/utils.h"
+#include "src/afficherMenu.h";
+
+
+int main ( int argc, char const **argv [] ) {
+    bool loop = true;
+
+    PAUSE ();
+    return EXIT_SUCCESS;
+}
