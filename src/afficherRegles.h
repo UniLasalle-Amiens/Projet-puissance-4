8a7a98c8512108c189b4b0987761d9876c013886
BREBION Mathis <mathis.brebion@icloud.com>
2023-01-06 17:05:52 +0100

    correction de qlq bugs d'affichage

--- src/afficherRegles.h
+++ src/afficherRegles.h
@@ -8,18 +8,24 @@ void afficherRegles () {
     printf ( "Fonctionnement du jeu:" );
     color ( RESET );
 
-    color ( ROUGE );
-
-    printf ( "\nLe programme vous demandera automatiquement avant chaque coup la ligne ainsi que la colonne où vous voulez jouer\n" );
+    printf ( "\n\nLe programme vous demandera automatiquement avant chaque coup la ligne ainsi que la colonne où vous voulez jouer\n" );
     printf ( "Le programme traite automatiquement toutes les regles du jeu et vous indiquera si le pion est jouable ou pas\n" );
 
     color ( RESET );
     
     color ( BLEU );
     printf ( "\n\nGagner une partie:" );
-    printf ( "\nAfin de gagner une partie, il suffit d'être le premier à aligner 4 jetons \nhorizontalemente, verticalementainsi que diagonalement" );
+
+    color ( RESET );
+
+    printf ( "\n\nAfin de gagner une partie, il suffit d'être le premier à aligner 4 jetons \nhorizontalemente, verticalementainsi que diagonalement" );
     
     color ( RESET );
+    
+    #ifndef WIN32
+        getchar ();
+    #endif
+
     PAUSE ();
 }
 
