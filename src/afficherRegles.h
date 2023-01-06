221c3f1ef0417ba180c850391f3f3a8601c6c65e
BREBION Mathis <mathis.brebion@icloud.com>
2023-01-06 17:01:14 +0100

    ajout des règles du jeu

--- src/afficherRegles.h
+++ src/afficherRegles.h
@@ -3,6 +3,24 @@
 
 void afficherRegles () {
     clear ();
+    
+    color ( BLEU );
+    printf ( "Fonctionnement du jeu:" );
+    color ( RESET );
+
+    color ( ROUGE );
+
+    printf ( "\nLe programme vous demandera automatiquement avant chaque coup la ligne ainsi que la colonne où vous voulez jouer\n" );
+    printf ( "Le programme traite automatiquement toutes les regles du jeu et vous indiquera si le pion est jouable ou pas\n" );
+
+    color ( RESET );
+    
+    color ( BLEU );
+    printf ( "\n\nGagner une partie:" );
+    printf ( "\nAfin de gagner une partie, il suffit d'être le premier à aligner 4 jetons \nhorizontalemente, verticalementainsi que diagonalement" );
+    
+    color ( RESET );
+    PAUSE ();
 }
 
 #endif
